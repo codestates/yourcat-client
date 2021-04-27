2dcec40df5f2b921fc81751b7b02d2dcddf4528d
Jebbit-koi <75734684+Jebbit-koi@users.noreply.github.com>
2021-04-28 01:49:29 +0900

    [#2][Add] Cat's criteria option

--- src/components/pages/CalculatorPage/Sections/InputSection.js
+++ src/components/pages/CalculatorPage/Sections/InputSection.js
@@ -1,30 +1,32 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import calculator from '../../../../_actions/calculator';
+import React, { useState, useRef } from 'react';
 
 export default function InputSection() {
-  const dispatch = useDispatch();
-  const [age, setAge] = useState('');
   const [weight, setWeight] = useState('');
+  const [calorie, setCalorie] = useState('');
+  const [feedWeight, setFeedWeight] = useState('');
   const [result, setResult] = useState('');
-
-  const onAgeHandler = event => {
-    setAge(event.currentTarget.value);
-  };
+  const catCriteria = useRef();
 
   const onWeightHandler = event => {
     setWeight(event.currentTarget.value);
   };
 
+  const onCalorieHandler = event => {
+    setCalorie(event.currentTarget.value);
+  };
+
+  const onFeedWeightHandler = event => {
+    setFeedWeight(event.currentTarget.value);
+  };
+
   const onSubmitHandler = event => {
     event.preventDefault();
     // 버튼을 누르면 그 값으로 계산해서 아웃풋으로 전달
-    // dispatch
-    dispatch(calculator(age));
 
-    setResult(age * weight);
-    setAge('');
+    setResult(calorie * weight);
+    setCalorie('');
     setWeight('');
+    console.log(catCriteria.current.value);
   };
 
   return (
@@ -34,16 +36,35 @@ export default function InputSection() {
 
         <input
           type="text"
-          value={age}
-          placeholder="age"
-          onChange={onAgeHandler}
-        />
-        <input
-          type="text"
           value={weight}
           placeholder="weight"
           onChange={onWeightHandler}
         />
+        <select ref={catCriteria}>
+          <option value="babycat">0 to 4 months </option>
+          <option value="kitten">4 months to Adult</option>
+          <option value="intactAdult">Intact adult</option>
+          <option value="neuteredAdult">Neutered adult</option>
+          <option value="inactiveAdult">Inactive adult</option>
+          <option value="activeAdult">Active adult</option>
+        </select>
+        <div>Please enter cat feed information.</div>
+        <div>
+          <input
+            type="text"
+            value={calorie}
+            placeholder="calorie"
+            onChange={onCalorieHandler}
+          />
+          kcal per
+          <input
+            type="text"
+            value={feedWeight}
+            placeholder="feedWeight"
+            onChange={onFeedWeightHandler}
+          />
+          g
+        </div>
 
         <button type="submit">Calculate</button>
       </form>
