5cb7e1bc167a29422efb92dab69b536cd89ee4f3
ShinYoungHo <dydh1230219@gmail.com>
2021-04-28 01:23:02 +0900

    [#5][Add] style and fix state

--- src/components/pages/LoginPage/LoginModal.js
+++ src/components/pages/LoginPage/LoginModal.js
@@ -1,15 +1,48 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
 import validation from '../../../utils/validator';
 import loginUserData from '../../../_actions/loginUser';
+
+const ButtonContainer = styled.div`
+  width: 200px;
+  box-sizing: border-box;
+`;
+const Button = styled.button`
+  width: 40%;
+  margin: 0 5%;
+`;
+const Container = styled.div`
+  border: 1px solid black;
+  display: flex;
+  width: 200px;
+  height: 20px;
+  background: ${props => (props.isValid ? `#FFD2D2` : 'none')};
+  border-radius: 2px;
+  margin: 1rem 0;
+`;
+const Input = styled.input`
+  all: unset;
+  flex: 1 1 0;
+  width: 100px;
+`;
+// position: absolute;
+const ErrMsg = styled.span`
+  position: absolute;
+  pointer-events: none;
+  left: 120px;
+  font-style: italic;
+  font-size: 0.5em;
+  color: #fd0202;
+`;
 
 const LoginModal = () => {
   const dispatch = useDispatch();
   const [loginInfo, setloginInfo] = useState({
     email: '',
     password: '',
-    isValid: { email: false, password: false },
   });
+  const [errMessage, setErrorMessage] = useState({ email: '', password: '' });
   const handleChange = key => e => {
     setloginInfo({
       ...loginInfo,
@@ -19,45 +52,55 @@ const LoginModal = () => {
   const handleBlur = key => e => {
     const isError = validation({ value: e.currentTarget.value, type: key });
     if (isError[key]) {
-      setloginInfo({
-        ...loginInfo,
-        isValid: { ...loginInfo.isValid, [key]: false },
-      });
+      setErrorMessage({ ...errMessage, [key]: isError[key] });
     } else {
-      setloginInfo({
-        ...loginInfo,
-        isValid: { ...loginInfo.isValid, [key]: true },
-      });
+      setErrorMessage({ ...errMessage, [key]: '' });
     }
   };
   const handleLogin = () => {
-    const { email, password } = loginInfo.isValid;
-    if (!email || !password) {
+    const { email, password } = errMessage;
+    if (email || password) {
       console.log('이메일과 비밀번호를 다시 확인해주세요');
     } else {
       const a = dispatch(loginUserData({ email, password }));
       console.log(a);
     }
   };
+  const handleSignUp = () => {};
   return (
     <>
+      <span>Login</span>
       <form onSubmit={e => e.preventDefault()}>
-        <input
-          key="email"
-          onBlur={handleBlur('email')}
-          onChange={handleChange('email')}
-          value={loginInfo.email || ''}
-        />
-        <input
-          key="password"
-          onBlur={handleBlur('password')}
-          onChange={handleChange('password')}
-          value={loginInfo.password || ''}
-        />
-        <button type="submit" onClick={handleLogin}>
-          Login
-        </button>
+        <Container isValid={errMessage.email}>
+          <Input
+            key="email"
+            onBlur={handleBlur('email')}
+            onChange={handleChange('email')}
+            value={loginInfo.email || ''}
+            placeholder="Email"
+          />
+          <ErrMsg>{errMessage.email ? errMessage.email : null}</ErrMsg>
+        </Container>
+        <Container isValid={errMessage.password}>
+          <Input
+            key="password"
+            type="password"
+            onBlur={handleBlur('password')}
+            onChange={handleChange('password')}
+            value={loginInfo.password || ''}
+            placeholder="Password"
+          />
+          <ErrMsg>{errMessage.password ? errMessage.password : null}</ErrMsg>
+        </Container>
       </form>
+      <ButtonContainer>
+        <Button type="button" onClick={handleSignUp}>
+          회원가입
+        </Button>
+        <Button type="submit" onClick={handleLogin}>
+          로그인
+        </Button>
+      </ButtonContainer>
     </>
   );
 };
