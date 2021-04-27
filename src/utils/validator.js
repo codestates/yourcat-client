0ec56bc2ca0584c03dc47d735a1ff623d0c301e2
ShinYoungHo <dydh1230219@gmail.com>
2021-04-28 01:23:58 +0900

    [Fix] error message

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -4,16 +4,24 @@ export default ({ value, type }) => {
     case 'email':
       if (type === 'email') {
         if (!value) {
-          errors.email = '이메일이 입력되지 않앗습니다.';
+          errors.email = '이메일을 입력해주세요.';
         } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
-          errors.email = '입력된 이메일이 유효하지 않습니다.';
+          errors.email = '이메일을 확인해주세요.';
         }
       }
       break;
-
     case 'password':
+      if (
+        !/^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/.test(
+          value,
+        )
+      ) {
+        errors.password = '비밀번호를 확인해주세요.';
+      }
+      break;
+    case 'signupPassword':
       if (!value) {
-        errors.password = '비밀번호가 입력되지 않았습니다.';
+        errors.password = '비밀번호를 입력해주세요.';
       } else if (value.length > 16 || value.length < 8) {
         errors.password = '8자 이상 16자 이하의 비밀번호를 입력해주세요';
       } else if (
@@ -25,6 +33,7 @@ export default ({ value, type }) => {
           '적어도 1자 이상의 숫자, 영문, 특수문자가 필요합니다.';
       }
       break;
+
     default:
       break;
   }
