5d6d3bdcdf01047c3755359c199abc4735541213
ShinYoungHo <dydh1230219@gmail.com>
2021-04-27 14:34:38 +0900

    [#3][Add] Blur event with validation

--- src/components/pages/LoginPage/LoginModal.js
+++ src/components/pages/LoginPage/LoginModal.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import validation from '../../../utils/validator';
 
 const LoginModal = () => {
   const [loginInfo, setloginInfo] = useState({
@@ -10,15 +11,23 @@ const LoginModal = () => {
       [key]: e.target.value,
     });
   };
+  const handleBlur = key => e => {
+    const isError = validation({ value: e.currentTarget.value, type: key });
+    if (isError[key]) {
+      console.log(isError);
+    }
+  };
   return (
     <>
       <input
         key="email"
+        onBlur={handleBlur('email')}
         onChange={handleChange('email')}
         value={loginInfo.email}
       />
       <input
         key="password"
+        onBlur={handleBlur('password')}
         onChange={handleChange('password')}
         value={loginInfo.password}
       />
