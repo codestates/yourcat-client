0de508c448ed5496f27993aa1ae1d8d9a2130a19
ShinYoungHo <dydh1230219@gmail.com>
2021-04-29 03:43:31 +0900

    [#11][Feat] interaction with db and fix component

--- src/_reducers/myInfo_reducer.js
+++
@@ -1,8 +0,0 @@
-export default function loginreducer(state = {}, action) {
-  switch (action.type) {
-    case 'MY_INFO':
-      return action.payload;
-    default:
-      return state;
-  }
-}
--- src/components/pages/LoginPage/LoginModal.js
+++ src/components/pages/LoginPage/LoginModal.js
@@ -1,44 +1,12 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import styled from 'styled-components';
 import validation from '../../../utils/validator';
-import { loginUser, getUserInfo } from '../../../_actions/loginUser';
-
-const ButtonContainer = styled.div`
-  width: 200px;
-  box-sizing: border-box;
-`;
-const Button = styled.button`
-  width: 40%;
-  margin: 0 5%;
-`;
-const Container = styled.div`
-  border: 1px solid black;
-  display: flex;
-  width: 200px;
-  height: 20px;
-  background: ${props => (props.isValid ? `#FFD2D2` : 'none')};
-  border-radius: 2px;
-  margin: 1rem 0;
-`;
-const Input = styled.input`
-  all: unset;
-  flex: 1 1 0;
-  width: 100px;
-`;
-const ErrMsg = styled.span`
-  position: absolute;
-  pointer-events: none;
-  left: 120px;
-  font-style: italic;
-  font-size: 0.5em;
-  color: #fd0202;
-`;
+import { loginUser, tokenHandler } from '../../../_actions/loginUser';
+import { Container, Input, ErrMsg } from '../../../utils/InputBox';
+import { ButtonContainer, Button } from '../../../utils/button';
 
 const LoginModal = React.memo(() => {
   const dispatch = useDispatch();
-  const history = useHistory();
   const [loginInfo, setloginInfo] = useState({
     email: '',
     password: '',
@@ -58,6 +26,9 @@ const LoginModal = React.memo(() => {
       setErrorMessage({ ...errMessage, [key]: '' });
     }
   };
+  const handleFocus = key => () => {
+    setErrorMessage({ ...errMessage, [key]: '' });
+  };
   const handleLogin = async () => {
     const { email, password } = loginInfo;
     if (errMessage.email || errMessage.password) {
@@ -65,47 +36,30 @@ const LoginModal = React.memo(() => {
     } else {
       dispatch(loginUser({ email, password })) //
         .then(({ payload: { data } }) => {
-          console.log(data);
-          // TODO : use Promise to handle login
-          if (true) {
-            // success
-            getUserInfo();
-          } else {
-            // TODO : loginInfo 초기화
-            setloginInfo({ email: '', password: '' });
-          }
+          dispatch(tokenHandler(data.accessToken));
         });
     }
   };
   const handleSignUp = () => {
-    // TODO : history.push something
-    history.push('/users/signup');
+    // TODO : history.push something or render another modal
   };
   return (
     <>
       <span>Login</span>
       <form onSubmit={e => e.preventDefault()}>
-        <Container isValid={errMessage.email}>
-          <Input
-            key="email"
-            onBlur={handleBlur('email')}
-            onChange={handleChange('email')}
-            value={loginInfo.email || ''}
-            placeholder="Email"
-          />
-          <ErrMsg>{errMessage.email ? errMessage.email : null}</ErrMsg>
-        </Container>
-        <Container isValid={errMessage.password}>
-          <Input
-            key="password"
-            type="password"
-            onBlur={handleBlur('password')}
-            onChange={handleChange('password')}
-            value={loginInfo.password || ''}
-            placeholder="Password"
-          />
-          <ErrMsg>{errMessage.password ? errMessage.password : null}</ErrMsg>
-        </Container>
+        {['email', 'password'].map(key => (
+          <Container key={key} isValid={errMessage[key]}>
+            <Input
+              type={key === 'password' ? 'password' : 'text'}
+              onBlur={handleBlur(key)}
+              onChange={handleChange(key)}
+              onFocus={handleFocus(key)}
+              value={loginInfo[key] || ''}
+              placeholder={key}
+            />
+            <ErrMsg>{errMessage[key] ? errMessage[key] : null}</ErrMsg>
+          </Container>
+        ))}
       </form>
       <ButtonContainer>
         <Button type="button" onClick={handleSignUp}>
