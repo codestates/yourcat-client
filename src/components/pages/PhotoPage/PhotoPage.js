840b39ba7faf0b9fc071cb31a38781d22d1fde54
Jebbit-koi <75734684+Jebbit-koi@users.noreply.github.com>
2021-05-01 01:34:05 +0900

    [#13][Update] PhotoPage

--- src/components/pages/PhotoPage/PhotoPage.js
+++ src/components/pages/PhotoPage/PhotoPage.js
@@ -1,60 +1,101 @@
 import React from 'react';
-import PhotoUploadForm from './Sections/PhotoUploadForm';
+import Grid from '@material-ui/core/Grid';
+
+import { makeStyles } from '@material-ui/core/styles';
+import PhotoCard from './Sections/PhotoCard';
+// import PhotoUploadForm from './Sections/PhotoUploadForm';
+import NavBar from '../LandingPage/Sections/NavBar';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
 
 export default function PhotoPage() {
+  const classes = useStyles();
+
   return (
-    <div style={{ margin: '100px' }}>
-      <PhotoUploadForm />
-      <div style={{ margin: '100px' }}>
-        <div
-          style={{
-            width: '300px',
-            height: '50px',
-            border: '1px solid',
-            padding: '3px',
-          }}
-        >
-          <img
-            src="http://placekitten.com/g/300/200"
-            alt="프로필"
-            style={{
-              width: '45px',
-              height: '45px',
-
-              borderRadius: '50%',
-            }}
-          />
-        </div>
-        <div
-          style={{
-            width: '300px',
-            height: '300px',
-            border: '1px solid',
-            padding: '5px',
-          }}
-        >
-          <img
-            src="http://placekitten.com/300/300"
-            alt="이미지"
-            style={{
-              width: '290px',
-              height: '290px',
-
-              borderRadius: '5%',
-            }}
-          />
-        </div>
-        <div
-          style={{
-            width: '300px',
-            height: '50px',
-            border: '1px solid',
-            padding: '17px',
-          }}
-        >
-          <h3>자는 모습도 귀여운 코이</h3>
-        </div>
+    <>
+      <NavBar />
+      <div
+        className={classes.root}
+        style={{
+          paddingTop: '100px',
+          padding: '50px',
+          backgroundColor: '#fcf9ea',
+        }}
+      >
+        <Grid container spacing={3}>
+          <Grid item lg={3} md={4} xs={12}>
+            <PhotoCard />
+          </Grid>
+          <Grid item lg={3} md={4} xs={12}>
+            <PhotoCard />
+          </Grid>
+          <Grid item lg={3} md={4} xs={12}>
+            <PhotoCard />
+          </Grid>
+          <Grid item lg={3} md={4} xs={12}>
+            <PhotoCard />
+          </Grid>
+          <Grid item lg={3} md={4} xs={12}>
+            <PhotoCard />
+          </Grid>
+        </Grid>
       </div>
-    </div>
+    </>
   );
 }
+
+// <PhotoUploadForm />
+//       <div style={{ margin: '100px' }}>
+//         <div
+//           style={{
+//             width: '300px',
+//             height: '50px',
+//             border: '1px solid',
+//             padding: '3px',
+//           }}
+//         >
+//           <img
+//             src="http://placekitten.com/g/300/200"
+//             alt="프로필"
+//             style={{
+//               width: '45px',
+//               height: '45px',
+
+//               borderRadius: '50%',
+//             }}
+//           />
+//         </div>
+//         <div
+//           style={{
+//             width: '300px',
+//             height: '300px',
+//             border: '1px solid',
+//             padding: '5px',
+//           }}
+//         >
+//           <img
+//             src="http://placekitten.com/300/300"
+//             alt="이미지"
+//             style={{
+//               width: '290px',
+//               height: '290px',
+
+//               borderRadius: '5%',
+//             }}
+//           />
+//         </div>
+//         <div
+//           style={{
+//             width: '300px',
+//             height: '50px',
+//             border: '1px solid',
+//             padding: '17px',
+//           }}
+//         >
+//           <h3>자는 모습도 귀여운 코이</h3>
+//         </div>
+//       </div>
--- src/components/pages/PhotoPage/Sections/PhotoUploadForm.js
+++ src/components/pages/PhotoPage/Sections/PhotoUploadForm.js
@@ -1,23 +1,211 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import propTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import fileHandler from '../../../../_actions/fileHandler';
 
-function PhotoUploadForm() {
+const Button = styled.button`
+  /* Insert your favorite CSS code to style a button */
+  all: unset;
+  margin: 0;
+  box-sizing: border-box;
+  text-align: center;
+  cursor: Pointer;
+  background: white;
+  border: 1px solid grey;
+  border-radius: 2px;
+  width: ${props => props.width - 2}px;
+  height: 20px;
+  top: ${props => props.height + 10}px;
+  position: absolute;
+  &:hover {
+    background-color: grey;
+    color: black;
+  }
+`;
+const Container = styled.section`
+  box-sizing: border-box;
+  border: 1px solid grey;
+  border-radius: ${props => props.border};
+  width: ${props => props.width}px;
+  height: ${props => props.height}px;
+`;
+function imgTagGenerator(width, height, border, src) {
   return (
-    <form>
-      <div>
-        <div
-          style={{ width: '300px', height: '300px', border: 'solid 1px black' }}
-        >
-          cat image
-        </div>
-        <button type="button">Photo Upload</button>
-      </div>
-      <div>
-        <h4>Title</h4>
-        <input />
-      </div>
-      <button type="submit">POST</button>
-    </form>
+    <img
+      alt=""
+      width={`${width}px`}
+      height={`${height}px`}
+      style={{
+        borderRadius: `${border}`,
+        position: 'absolute',
+      }}
+      src={src}
+    />
   );
 }
 
+const PhotoUploadForm = props => {
+  const { width, height, border } = props;
+  const [title, setTitle] = useState('');
+
+  const [file, setFile] = useState('');
+  const [previewURL, setPreviewURL] = useState('');
+  const [preview, setPreview] = useState(null);
+  const [image, setImage] = useState('');
+  const dispatch = useDispatch();
+  const hiddenFileInput = React.useRef(null);
+
+  const onTitleHandler = event => {
+    setTitle(event.currentTarget.value);
+  };
+
+  useEffect(() => {
+    if (file !== '') {
+      setPreview(imgTagGenerator(width, height, border, previewURL));
+    }
+  }, [previewURL]);
+
+  const handleChange = async event => {
+    const reader = new FileReader();
+    const formData = new FormData();
+    const fileData = event.target.files[0];
+    formData.append('image', fileData);
+
+    const response = await axios.post(
+      'http://localhost:4000/images/upload',
+      formData,
+    );
+    console.log('response: ', response);
+    console.log('파일경로: ', response.data.filePath);
+    const truePath = response.data.filePath.split('/')[1];
+
+    setImage(truePath);
+    console.log('fileData: ', fileData);
+
+    reader.onloadend = () => {
+      setFile(fileData);
+      dispatch(fileHandler(fileData));
+      setPreviewURL(reader.result);
+    };
+
+    if (fileData) {
+      reader.readAsDataURL(fileData);
+    }
+  };
+
+  const handleClick = () => {
+    hiddenFileInput.current.click();
+  };
+
+  return (
+    <Container width={width} height={height} border={border}>
+      {preview}
+      <Button onClick={handleClick} width={width} height={height}>
+        Upload Photo
+      </Button>
+      <label htmlFor="file">
+        <input
+          type="file"
+          accept=".gif, .jpg, .png"
+          ref={hiddenFileInput}
+          onChange={handleChange}
+          style={{ display: 'none' }}
+        />
+      </label>
+      <div>
+        <img
+          style={{
+            width: '300px',
+            height: '240px',
+            position: 'absolute',
+            right: '50px',
+          }}
+          alt="df"
+          src={`http://localhost:4000/${image}`}
+        />
+      </div>
+      <form>
+        <div>
+          <h4>Title</h4>
+          <input
+            type="text"
+            value={title}
+            placeholder="title"
+            onChange={onTitleHandler}
+          />
+        </div>
+        <br />
+        <br />
+        <button type="submit">POST</button>
+      </form>
+    </Container>
+  );
+};
+
+PhotoUploadForm.propTypes = {
+  width: propTypes.number.isRequired,
+  height: propTypes.number.isRequired,
+  border: propTypes.string.isRequired,
+};
+
 export default PhotoUploadForm;
+
+// // import FileUploader from '../../../../utils/ImageUploader';
+
+// function PhotoUploadForm() {
+//   const [title, setTitle] = useState('');
+
+//   const onTitleHandler = event => {
+//     setTitle(event.currentTarget.value);
+//   };
+
+//   const dropHandler = photos => {
+//     const formData = new FormData();
+//     const config = {
+//       header: { 'content=type': 'multipart/form-data' },
+//     };
+//     formData.append('photo', photos[0]);
+
+//     axios.post('/contents/create', formData, config).then(response => {
+//       if (response.data.success) {
+//         alert('성공');
+//       } else {
+//         alert('파일을 저장하는데 실패했습니다');
+//       }
+//     });
+//   };
+
+//   return (
+//     <>
+//       <div>
+//         <FileUploader
+//           onDrop={dropHandler}
+//           width={300}
+//           height={300}
+//           border="10px"
+//         />
+//       </div>
+//       <br />
+//       <br />
+//       <br />
+//       <form>
+//         <div>
+//           <h4>Title</h4>
+//           <input
+//             type="text"
+//             value={title}
+//             placeholder="title"
+//             onChange={onTitleHandler}
+//           />
+//         </div>
+//         <br />
+//         <br />
+//         <button type="submit">POST</button>
+//       </form>
+//     </>
+//   );
+// }
+
+// export default PhotoUploadForm;
