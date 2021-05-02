c6ca3bb9db7e4c3a96a0b909d3f92f029970b8e0
YU-WON <shallwedance0419@gmail.com>
2021-05-02 20:09:43 +0900

    [#25][Feat] Detail Page

--- src/components/pages/CommunityPage/Sections/DetailContents.js
+++ src/components/pages/CommunityPage/Sections/DetailContents.js
@@ -1,39 +1,113 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
 axios.defaults.withCredentials = true;
 
-function DetailContents() {
-  const [title, setTitle] = useState('');
-  const [category, setCategory] = useState('');
-  const [description, setDescription] = useState('');
-  const [like, setLike] = useState('');
-  const [comment, setComment] = useState([]);
+const TITLE = styled.div`
+  display: flex;
+  padding: 50px;
+  border: none;
+  margin: 50px auto;
+  width: 70%;
+  background: rgba(0, 0, 0, 0.003);
+  box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03);
+  font-weight: 300;
+  font-size: 40px;
+`;
 
-  console.log(title);
-  console.log(category);
-  console.log(description);
-  console.log(like);
-  console.log(comment);
+const DIV = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 80%;
+  padding: 5px;
+  background: rgba(0, 0, 0, 0.003);
+  font-size: 15px;
+`;
+
+const NICKNAME = styled.div`
+  flex: none;
+  border: none;
+  margin: 17px 0 0 300px;
+  margin-rigth: auto;
+  background: rgba(0, 0, 0, 0.003);
+  font-size: 17px;
+`;
+
+const LIKEBNT = styled.button`
+  margin: 7px;
+  margin-left: auto;
+  border: 1px;
+  border: 1px solid #bdbdbd;
+  border-radius: 10px;
+  font-size: 17px;
+  background-color: #f5f5f5;
+`;
+
+const LIKE = styled.div`
+  border: none;
+  margin: 15px;
+  background: rgba(0, 0, 0, 0.003);
+  font-size: 17px;
+`;
+
+const DESCRIPTION = styled.div`
+  display: flex;
+  padding: 16px;
+  border: none;
+  width: 70%;
+  margin: 50px auto;
+  background: rgba(0, 0, 0, 0.003);
+  box-shadow: inset 0 2px 1px rgba(0, 0, 0, 0.03);
+  font-weight: 300;
+  font-size: 25px;
+`;
+
+function DetailContents() {
+  const [{ title, description, like, userId }, setContentData] = useState('');
+
+  const [likeSwitch, setLikeSwitch] = useState(false);
+
+  useEffect(() => {
+    console.log('likeSwitch', likeSwitch);
+    if (likeSwitch) {
+      setContentData({ title, description, userId, like: like + 1 });
+    } else {
+      setContentData({ title, description, userId, like: like - 1 });
+    }
+  }, [likeSwitch]);
 
   const { contentId } = useParams();
-  const url = `http://localhost:3000/contents/detail/${contentId}`;
-  axios
-    .get(url)
-    .then(response => {
-      const set = response.contentInfo;
-      setTitle(set.title);
-      setCategory(set.category);
-      setDescription(set.description);
-      setLike(set.like);
-      setComment().concat(set.comment);
-    })
-    .catch(err => {
-      console.log(err);
-    });
 
-  return <div>Hi</div>;
+  useEffect(() => {
+    const url = `http://localhost:4000/contents/detail/${contentId}`;
+    axios
+      .get(url)
+      .then(response => {
+        // console.log('res.data.contentInfo ', response.data.contentInfo);
+        setContentData(response.data.contentInfo);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      <TITLE>{title}</TITLE>
+      <DIV>
+        {userId !== undefined ? (
+          <NICKNAME>😸Nickname: {userId.nickname}</NICKNAME>
+        ) : (
+          <> </>
+        )}
+        <LIKEBNT onClick={() => setLikeSwitch(!likeSwitch)}>Like👍 </LIKEBNT>
+        <LIKE>{like}</LIKE>
+      </DIV>
+      <DESCRIPTION>{description}</DESCRIPTION>
+    </>
+  );
 }
 
 export default DetailContents;
