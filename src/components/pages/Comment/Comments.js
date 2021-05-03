c71065b1316c3ec4e3c40d056eea7314c61c5a8c
Jebbit-koi <75734684+Jebbit-koi@users.noreply.github.com>
2021-05-03 11:31:31 +0900

    [#27][Fix] change component structure

--- src/components/pages/Comment/Comments.js
+++ src/components/pages/Comment/Comments.js
@@ -1,19 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
 import axios from 'axios';
-
-const Writer = styled('h6')`
-  padding: 5px;
-  color: grey;
-`;
-
-const Content = styled('h4')`
-  padding: 5px;
-`;
-
-const Button = styled('button')`
-  margin: 0 10px;
-`;
+import SingleComment from './SingleComment';
 
 function Comments() {
   const [comment, setComment] = useState('');
@@ -23,12 +10,13 @@ function Comments() {
     setComment(event.currentTarget.value);
     console.log(comment);
   };
+
   useEffect(() => {
     axios
       .get('http://localhost:4000/contents/detail/608e665af74f883c54bc72e2')
       .then(response => {
         console.log('responseData는 ', response.data);
-        console.log('comment는 ', response.data.contentInfo.comment);
+        console.log('comment ', response.data.contentInfo.comment);
         setCommentList(response.data.contentInfo.comment);
       });
   }, []);
@@ -62,25 +50,13 @@ function Comments() {
   // TODO: 본인이 쓴 댓글이면 수정, 삭제 버튼 나타나도록
   return (
     <div style={{ margin: '50px 20px' }}>
-      <div style={{ fontSize: '20px', color: 'green' }}>Comments</div>
+      <div style={{ fontSize: '20px', color: '#f8a978' }}>Comments</div>
       <br />
       <br />
-      {commentList &&
-        commentList.map(ele => (
-          <div style={{ display: 'flex' }}>
-            <img alt="프사" style={{ margin: '10px' }} />
-            <div>
-              <div style={{ display: 'flex' }}>
-                <Writer>코이언니</Writer>
-                <Button>수정</Button>
-                <Button>삭제</Button>
-              </div>
-              <div>
-                <Content>{ele.description}</Content>
-              </div>
-            </div>
-          </div>
-        ))}
+      <div>
+        {commentList &&
+          commentList.map(ele => <SingleComment comment={ele.description} />)}
+      </div>
 
       <br />
       <br />
