ac74a89fcd12ffaf128467a1510d92a05f5b996d
Jebbit-koi <75734684+Jebbit-koi@users.noreply.github.com>
2021-05-03 14:40:36 +0900

    [#37][Style] CalculatorPage design

--- src/components/pages/CalculatorPage/Sections/Header.js
+++ src/components/pages/CalculatorPage/Sections/Header.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const HEADER = styled('div')`
+  margin: 10px 100px;
+  margin-top: 50px;
+  padding: 10px;
+  color: #f8a978;
+  font-size: 30px;
+`;
 
 export default function Header() {
-  return <div>This is header</div>;
+  return <HEADER>Feeding Calculator</HEADER>;
 }
--- src/components/pages/CalculatorPage/Sections/OutputSection.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function OutputSection() {
-  return <div>OutputSection</div>;
-}
