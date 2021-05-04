77c1a653697554a8f594e91c83487ac0f2109e0e
ShinYoungHo <dydh1230219@gmail.com>
2021-05-04 10:14:29 +0900

    [#48][Add] login check with auth

--- src/_reducers/users/login_reducer.js
+++ src/_reducers/users/login_reducer.js
@@ -3,7 +3,7 @@ export default function loginreducer(state = '', action) {
     case 'USER_LOGIN':
       return action.payload;
     case 'STORE_TOKEN':
-      return action.payload;
+      return action.payload || state;
     case 'GET_TOKEN':
       return state;
     case 'DELETE_TOKEN':
--- src/utils/Hook/useCheckToken.js
+++ src/utils/Hook/useCheckToken.js
@@ -1,27 +1,77 @@
-import { useEffect, useState } from 'react'; // useEffect,
-import { useSelector } from 'react-redux';
-
+import { useState } from 'react'; // useEffect,
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import { tokenHandler } from '../../_actions/users/loginUser';
 // --------------------------
 // import useCheckToken from 'somewhere';
 //
 // const [{ result }, setResult] = useCheckToken();
 // setResult();
 // 실행시 result = token || false
+// const [x, setResult] = useCheckToken();
+// const handleClick = () => {
+//   setResult();
+//   x.isAuth가 true이면 수정기능으로 넘어갈 수 있다.
+// };
+// useEffect(() => {
+//   setResult();
+// }, []);
+// console.log(x);
 // --------------------------
+
+function checkAuth(token) {
+  if (!token) return false;
+  return axios
+    .get('http://localhost:4000/users/authcheck', {
+      headers: { authorization: `Bearer ${token}` },
+    })
+    .then(({ data }) => {
+      return data;
+    })
+    .catch(err => err);
+}
+
 export default () => {
+  const dispatch = useDispatch();
+  const [data, setData] = useState({ result: '' });
   const result = useSelector(dat => {
     return dat.login ? dat.login : false;
   });
-  const [data, setData] = useState({ result });
-  useEffect(() => {
-    setData({ result });
-  }, [result]);
-  const handler = () => {
-    setData({ result });
+  const handler = async () => {
+    console.log(result);
+    const checkedAuth = await checkAuth(result);
+    if (checkedAuth.isAuth) {
+      dispatch(tokenHandler(result));
+    }
+    setData({ result: checkedAuth });
   };
-
   return [data, handler];
 };
+
+// export default () => {
+//   console.log(1);
+//   const dispatch = useDispatch();
+//   const result = useSelector(dat => {
+//     return dat.login ? dat.login : false;
+//   });
+//   const [init, setInit] = useState('');
+//   const [data, setData] = useState({ result: '' });
+//   console.log(2);
+//   useEffect(async () => {
+//     const checkedAuth = await checkAuth(result);
+//     if (checkedAuth.isAuth) {
+//       dispatch(tokenHandler(data.accessToken));
+//     }
+//     setData({ result: checkedAuth });
+//   }, [init]);
+//   console.log(3);
+//   const handler = () => {
+//     console.log(4);
+//     setInit(Math.random());
+//   };
+
+//   return [data, handler];
+// };
 
 // export default () => {
 //   const [random, setRandom] = useState();
