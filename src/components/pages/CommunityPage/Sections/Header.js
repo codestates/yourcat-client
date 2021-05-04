4fc0202f7193645cb11fa41e2998c4dc1fa16fef
YU-WON <shallwedance0419@gmail.com>
2021-05-04 16:37:58 +0900

    [#52][FIX] Bug fixes

---
+++ src/components/pages/CommunityPage/Sections/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HEADER = styled('div')`
+  margin: 10px 150px;
+  margin-top: 50px;
+  padding: 10px;
+  color: #f8a978;
+  font-size: 30px;
+`;
+
+export default function Header() {
+  return <HEADER>Community</HEADER>;
+}
