81f12228de341224612b6f55c2046e6741dd3564
ShinYoungHo <dydh1230219@gmail.com>
2021-05-05 11:08:15 +0900

    [#64][Add] logout

--- src/_reducers/users/getuserinfo_reducer.js
+++ src/_reducers/users/getuserinfo_reducer.js
@@ -2,6 +2,8 @@ export default function getUsersInfo(state = '', action) {
   switch (action.type) {
     case 'GET_USERINFO':
       return action.payload;
+    case 'DELETE_USERINFO':
+      return '';
     default:
       return state;
   }
