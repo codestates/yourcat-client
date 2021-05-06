3ba51efe2541d4f6d325f2081592523bea6adf2d
Jebbit-koi <75734684+Jebbit-koi@users.noreply.github.com>
2021-05-06 20:41:25 +0900

    [#119][Style] edit info

--- src/components/pages/Mypage/Sections/EditInfo.js
+++ src/components/pages/Mypage/Sections/EditInfo.js
@@ -5,20 +5,113 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import useCheckToken from '../../../../utils/Hook/useCheckToken';
 import getUserInfo from '../../../../_actions/users/getUserInfo';
+import { MODAL, HeaderBox, HEADER } from '../../../../utils/ModalHeader';
+import { Button, ButtonContainer } from '../../../../utils/button';
 
-const DIV = styled.div`
+const INPUTDIV = styled('div')`
   display: flex;
+  align-items: center;
+  justify-content: center;
   flex-direction: column;
+  height: 50%;
 `;
-const INPUT = styled.input`
-  display: flex;
-  flex-direction: column;
+
+const Input = styled.input`
+  all: unset;
   width: 60%;
-  height: 30%;
-  padding: 50px;
-  margin-top: 200px;
-  margin-left: auto;
+  margin: 0 10px;
+  border-bottom: 1px solid rgb(0, 0, 0, 0.3);
+  text-align: center;
+  &:hover {
+    border-bottom: 1px solid #badfdb;
+  }
 `;
+
+const NickBOX = styled('div')`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 90%;
+  margin-top: 40px;
+`;
+
+const NickInput = styled.input`
+  all: unset;
+  width: 30%;
+  margin-right: 10px;
+
+  border-bottom: 1px solid rgb(0, 0, 0, 0.3);
+  text-align: center;
+  &:hover {
+    border-bottom: 1px solid #badfdb;
+  }
+`;
+
+const BUTTON = styled.button`
+  padding: 5px 0;
+  width: 25%;
+  margin-right: 10px;
+  background-color: #badfdb;
+  color: white;
+  border-radius: 7px;
+  font-size: 17px;
+  border: none;
+  &:hover {
+    background-color: #94d4cd;
+  }
+`;
+
+const SELECT = styled.select`
+  all: unset;
+  width: 60%;
+  margin: 0 10px;
+  text-align-last: center;
+
+  border: none;
+  border-bottom: 1px solid rgb(0, 0, 0, 0.3);
+
+  &:hover {
+    border-bottom: 1px solid #badfdb;
+  }
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 200%;
+  background-color: rgb(0, 0, 0, 0);
+  background-color: rgb(0, 0, 0, 0.2);
+  position: fixed;
+  margin: auto;
+  top: 0;
+  z-index: 10;
+`;
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 25%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 20px;
+  width: 420px;
+  height: 500px;
+  border-radius: 5px;
+  background-color: #fcf9ea;
+  align-items: center;
+`;
+const TITLE = styled('div')`
+  margin: 10px 0;
+  width: 40%;
+`;
+
+const INPUTBOX = styled('div')`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 90%;
+`;
+
 const EditInfo = React.memo(({ catInfo, nickname, setIsEdit, email }) => {
   const [cat, setCat] = useState(catInfo);
   const [edit, setEdit] = useState({ nickname, email });
@@ -108,33 +201,61 @@ const EditInfo = React.memo(({ catInfo, nickname, setIsEdit, email }) => {
   };
   //   console.log(history);
   return (
-    <form onSubmit={event => event.preventDefault}>
-      <DIV>Cat Name: </DIV>
-      <INPUT onChange={handleChange('name')} type="text" value={cat.name} />
-      <DIV>Gender: </DIV>
-      {/* <INPUT onChange={handleChange('gender')} type="text" value={cat.gender} /> */}
-      <select onChange={handleChange('gender')}>
-        <option value="male"> Male </option>
-        <option value="female"> Female </option>
-      </select>
-      <DIV>Age: </DIV>
-      <INPUT onChange={handleChange('age')} type="number" value={cat.age} />
-      <DIV>Nickname: </DIV>
-      <INPUT
-        onChange={handleChange('nickname')}
-        type="text"
-        value={edit.nickname}
-      />
-      <button type="button" onClick={switchIsEdit}>
-        CANCEL
-      </button>
-      <button type="button" onClick={editSubmit}>
-        EDIT
-      </button>
-      <button type="button" onClick={nickNameCheck}>
-        Duplicate check
-      </button>
-    </form>
+    <Wrapper>
+      <Main>
+        <MODAL onSubmit={event => event.preventDefault}>
+          <HeaderBox>
+            <HEADER>Edit Info</HEADER>
+          </HeaderBox>
+          <INPUTDIV>
+            <INPUTBOX>
+              <TITLE>Cat Name: </TITLE>
+              <Input
+                onChange={handleChange('name')}
+                type="text"
+                value={cat.name}
+              />
+            </INPUTBOX>
+
+            <INPUTBOX>
+              <TITLE>Cat Gender: </TITLE>
+              {/* <Input onChange={handleChange('gender')} type="text" value={cat.gender} /> */}
+              <SELECT onChange={handleChange('gender')}>
+                <option value="male"> Male </option>
+                <option value="female"> Female </option>
+              </SELECT>
+            </INPUTBOX>
+            <INPUTBOX>
+              <TITLE>Cat Age: </TITLE>
+              <Input
+                onChange={handleChange('age')}
+                type="number"
+                value={cat.age}
+              />
+            </INPUTBOX>
+            <NickBOX>
+              <TITLE>Nickname: </TITLE>
+              <NickInput
+                onChange={handleChange('nickname')}
+                type="text"
+                value={edit.nickname}
+              />
+              <BUTTON type="button" onClick={nickNameCheck}>
+                중복 체크
+              </BUTTON>
+            </NickBOX>
+          </INPUTDIV>
+          <ButtonContainer>
+            <Button type="button" onClick={switchIsEdit}>
+              취소
+            </Button>
+            <Button type="button" onClick={editSubmit}>
+              수정
+            </Button>
+          </ButtonContainer>
+        </MODAL>
+      </Main>
+    </Wrapper>
   );
 });
 EditInfo.propTypes = {
